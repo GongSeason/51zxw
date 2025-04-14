8f921c098fce703e054b2d8b1567e37b50d1fb08
Season <2492596620@qq.com>
2025-04-14 20:00:09 +0800

    update

---
+++ C#_test_for_fun/类型参数T/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 类型参数T
+{
+    // 定义泛型类
+    public class TGenericDemo<T>
+    {
+        void Exp(T input)
+        {
+
+        }
+    }
+    internal class Program
+    {
+        private class customClass
+        {
+
+        }
+        static void Main(string[] args)
+        {
+            // 创建泛型类的实例
+            TGenericDemo<char> gd1 = new TGenericDemo<char>();
+            Console.WriteLine("实例化类型为char的TGenericDemo的gd1成功！");
+
+            TGenericDemo<int> gd2 = new TGenericDemo<int>();
+            Console.WriteLine("实例化类型为int的TGenericDeom的gd2成功");
+
+            TGenericDemo<customClass> gd3 = new TGenericDemo<customClass>();
+            Console.WriteLine("实例化类型为customClass的TGenericDemo的gd3成功");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C#_test_for_fun/自定义泛型类船舰/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 自定义泛型类创建
+{
+
+    public class TGeneric<T>
+    {
+        public T Field;
+
+    }
+
+    class commonlyClass
+    {
+        public T G<T>(T t)
+        {
+            return t;
+        }
+    }
+
+    class TGenericTwo<T>
+    {
+        public T M(T t) //泛型方法
+        {
+            return t;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TGeneric<string> g = new TGeneric<string>();
+            g.Field = "Hello World";
+            Console.WriteLine(g.Field);
+            Console.ReadKey();
+        }
+    }
+}
